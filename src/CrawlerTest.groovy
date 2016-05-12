fc1cb7ababb9ea59c522d3af820f29e7dbd16bfd
Khama <khastick05@gmail.com>
2016-05-12 15:47:41 -0400

    Added a testing class for crawler.

---
+++ src/CrawlerTest.groovy
@@ -0,0 +1,20 @@
+/**
+ * Created by Khama on 2016-05-12.
+ */
+class CrawlerTest extends GroovyTestCase {
+    void tearDown() {
+
+    }
+
+    void testProcess() {
+
+    }
+
+    void testProcessLink() {
+
+    }
+
+    void testProcessFile() {
+
+    }
+}
